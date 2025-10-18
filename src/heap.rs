4d5791bb85cc839f88ae03c4ffda97c27b34526e
dragonn <dragonn@op.pl>
2025-10-18 22:18:38 +0200

    heap task logging

--- src/heap.rs
+++ src/heap.rs
@@ -3,8 +3,8 @@ use embassy_time::{Duration, Timer};
 #[embassy_executor::task]
 pub async fn heap_task() {
     loop {
-        Timer::after(Duration::from_secs(60)).await;
         let heap_stats = esp_alloc::HEAP.stats();
         info!("Heap stats - total: {:?}", heap_stats);
+        Timer::after(Duration::from_secs(5)).await;
     }
 }
