209a13e16cd2d0307581e1dfea69c3d7128dd9dd
dragonn <dragonn@op.pl>
2025-10-18 22:32:22 +0200

    adjust heap size logging

--- src/heap.rs
+++ src/heap.rs
@@ -4,7 +4,13 @@ use embassy_time::{Duration, Timer};
 pub async fn heap_task() {
     loop {
         let heap_stats = esp_alloc::HEAP.stats();
-        info!("Heap stats - total: {:?}", heap_stats);
+        let current_percentage = heap_stats.current_usage as f32 / heap_stats.size as f32 * 100.0;
+        let max_percentage = heap_stats.max_usage as f32 / heap_stats.size as f32 * 100.0;
+        //info!("Heap stats - total: {:?}", heap_stats);
+        info!(
+            "Heap usage - current: {} bytes ({:.2}%), max: {} bytes ({:.2}%)",
+            heap_stats.current_usage, current_percentage, heap_stats.max_usage, max_percentage
+        );
         Timer::after(Duration::from_secs(5)).await;
     }
 }
