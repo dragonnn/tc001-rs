79bee875b99446c5662d190f6c61dde430d8cbae
dragonn <dragonn@op.pl>
2025-10-18 22:32:49 +0200

    debug ntp offsets

--- src/ntp/mod.rs
+++ src/ntp/mod.rs
@@ -1,4 +1,5 @@
 use embassy_net::IpEndpoint;
+use embassy_time::Instant;
 
 use crate::udp::UdpBuffers;
 
@@ -20,10 +21,8 @@ async fn ntp_request(stack: embassy_net::Stack<'static>) -> Result<chrono::Naive
     let mut socket = embassy_net::udp::UdpSocket::new(stack, rx_meta, rx_buffer, tx_meta, tx_buffer);
 
     socket.bind(11770).map_err(|e| ())?;
-    info!("UDP socket bound");
 
     socket.send_to(&raw_ntp.0, IpEndpoint::new(addr, 123)).await.map_err(|e| ())?;
-    info!("NTP request sent");
 
     let mut buffer = [0u8; 48];
 
@@ -59,11 +58,30 @@ async fn ntp_request(stack: embassy_net::Stack<'static>) -> Result<chrono::Naive
 
 #[embassy_executor::task]
 pub async fn ntp_task(stack: embassy_net::Stack<'static>, rtc: &'static esp_hal::rtc_cntl::Rtc<'static>) {
+    let mut last_ntp_date = None;
+    let mut now = Instant::now();
     loop {
         match ntp_request(stack.clone()).await {
             Ok(date) => {
                 info!("NTP date: {:?}", date);
+                let rtc_now = chrono::NaiveDateTime::from_timestamp_micros(rtc.current_time_us() as i64).unwrap();
+                let rtc_delta = date.signed_duration_since(rtc_now);
+                info!("RTC delta: {} seconds", rtc_delta.as_seconds_f64());
+
                 rtc.set_current_time_us(date.and_utc().timestamp_micros() as u64);
+
+                if let Some(last_date) = last_ntp_date {
+                    let delta = date.signed_duration_since(last_date);
+                    let elapsed = now.elapsed();
+                    info!(
+                        "Time since last NTP: {} seconds (RTC delta: {} seconds)",
+                        elapsed.as_millis() as f32 / 1000.0,
+                        delta.as_seconds_f64()
+                    );
+                }
+
+                now = Instant::now();
+                last_ntp_date = Some(date);
             }
             Err(_) => {
                 error!("NTP request failed");
