855acd113d3d6174fd6df112ff8e050827a1f922
dragonn <dragonn@op.pl>
2025-10-29 08:11:08 +0100

    improve adc reading

--- src/adc.rs
+++ src/adc.rs
@@ -5,27 +5,20 @@ use esp_hal::gpio::Input;
 #[embassy_executor::task]
 pub async fn adc_task(mut adc: super::Adc, mut battery: super::BatteryPin, mut light_sensor: super::LightSensorPin) {
     loop {
-        let battery_adc = adc.read_oneshot(&mut battery);
-        match battery_adc {
-            Ok(value) => {
-                info!("read {} battery ADC value", value);
-                let percentage = battery_adc_to_percentage(value);
-                info!("Battery: {:.2} %", percentage);
-                info!("Battery ADC value: {}", value)
-            }
-            Err(e) => warn!("Failed to read battery ADC: {:?}", e),
-        }
-
         for i in 0..10 {
-            let light_adc = adc.read_oneshot(&mut light_sensor);
-            match light_adc {
-                Ok(value) => {
-                    info!("read {} light sensor ADC value", value);
-                }
-                Err(e) => warn!("Failed to read light sensor ADC: {:?}", e),
-            }
-            Timer::after(Duration::from_secs(1)).await;
+            let battery_adc = read_oneshot(&mut adc, &mut battery).await;
+
+            info!("read {} battery ADC value", battery_adc);
+            let percentage = battery_adc_to_percentage(battery_adc);
+            info!("bat: {:.2} %", percentage);
+
+            let light_adc = read_oneshot(&mut adc, &mut light_sensor).await;
+
+            info!("read {} light sensor ADC value", light_adc);
+
+            Timer::after(Duration::from_millis(100)).await;
         }
+        Timer::after(Duration::from_secs(10)).await;
     }
 }
 
@@ -35,4 +28,26 @@ fn battery_adc_to_percentage(adc_value: u16) -> f32 {
     let percentage = (adc_value - 3.0) / 0.69 * 100.0;
     info!("percentage: {}", percentage);
     percentage.max(100.0).min(0.0)
+}
+
+fn scale_12_to_10_exact(x: u16) -> u16 {
+    let x = (x & 0x0FFF) as u32;
+    ((x * 1023 + 2047) / 4095) as u16
+}
+
+pub async fn read_oneshot<'d, PIN>(
+    adc: &mut super::Adc,
+    pin: &mut esp_hal::analog::adc::AdcPin<PIN, esp_hal::peripherals::ADC1<'static>>,
+) -> u16
+where
+    PIN: esp_hal::analog::adc::AdcChannel,
+{
+    loop {
+        match adc.read_oneshot(pin) {
+            Ok(value) => return scale_12_to_10_exact(value),
+            Err(_) => {
+                Timer::after(Duration::from_millis(10)).await;
+            }
+        }
+    }
 }
