831198dc0d83890b603d690fb01d27afe978f267
dragonn <dragonn@op.pl>
2025-12-10 21:42:47 +0100

    add ds1307 driver

--- ds1307/src/lib.rs
+++ ds1307/src/lib.rs
@@ -1,17 +1,11 @@
-#![cfg_attr(docsrs, feature(doc_auto_cfg))]
 #![cfg_attr(not(test), no_std)]
-#![deny(unsafe_code)]
-#![warn(missing_docs)]
-#![cfg_attr(not(doctest), doc = include_str!("../README.md"))]
 
-pub mod async_api;
-pub mod control;
-pub mod datetime;
+mod async_api;
+mod control;
+mod datetime;
 mod ds1307;
-pub mod error;
-pub mod nvram;
-pub mod registers;
-pub mod square_wave;
+mod error;
+mod registers;
 
+pub use async_api::{AsyncRtc, AsyncRtcNvram, AsyncRtcPowerControl, AsyncSquareWave};
 pub use ds1307::Ds1307;
-pub use rtc_hal::datetime::DateTime;
--- src/ds1307.rs
+++ src/ds1307.rs
@@ -1,3 +1,4 @@
+use ds1307::{AsyncRtc as _, AsyncRtcPowerControl as _};
 use embassy_futures::select::{select3, Either3};
 use embassy_time::{Duration, Timer};
 use embedded_hal_async::i2c::I2c;
@@ -7,10 +8,21 @@ use esp_hal::gpio::Input;
 pub async fn ds1307_task(mut i2c0: &'static crate::I2c0) {
     Timer::after(Duration::from_millis(5000)).await; // wait for other init
     let mut i2c_device = embassy_embedded_hal::shared_bus::asynch::i2c::I2cDevice::new(&mut i2c0);
+    let mut ds1307 = ds1307::Ds1307::new(i2c_device);
+    ds1307.start_clock().await.unwrap();
     loop {
-        let mut buffer = [0u8; 3];
-        i2c_device.write_read(0x68, &[0x00], &mut buffer).await.unwrap();
-        info!("readed: {:02x} {:02x} {:02x}", buffer[0], buffer[1], buffer[2]);
+        //let mut buffer = [0u8; 3];
+        //i2c_device.write_read(0x68, &[0x00], &mut buffer).await.unwrap();
+        let datetime = ds1307.get_datetime().await.unwrap();
+        info!(
+            "DS1307 DateTime: {:04}-{:02}-{:02} {:02}:{:02}:{:02}",
+            datetime.year(),
+            datetime.month(),
+            datetime.day_of_month(),
+            datetime.hour(),
+            datetime.minute(),
+            datetime.second()
+        );
         Timer::after(Duration::from_millis(1000)).await;
     }
 }
