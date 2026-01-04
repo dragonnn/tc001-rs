c5e632b0fa27ae5c779dad6401add9cd0591ac0f
dragonn <dragonn@op.pl>
2026-01-04 19:51:51 +0100

    more embassy-ha prepare

--- src/ha.rs
+++ src/ha.rs
@@ -17,33 +17,67 @@ static RESOURCES: StaticCell<embassy_ha::DeviceResources> = StaticCell::new();
 #[embassy_executor::task]
 pub async fn ha_task(spawner: Spawner, stack: embassy_net::Stack<'static>) {
     crate::wifi::wait_for_connection(&stack).await;
-    Timer::after(embassy_time::Duration::from_secs(5)).await;
+    Timer::after(embassy_time::Duration::from_secs(1)).await;
 
     let mac = esp_radio::wifi::station_mac();
     info!("Device MAC address: {:02X?}", mac);
 
     let mut device_id = String::with_capacity(13);
 
-    write!(&mut device_id, "awtrix_{:02x}{:02x}{:02x}", mac[3], mac[4], mac[5]).ok();
+    write!(&mut device_id, "rwtrix_{:02x}{:02x}{:02x}", mac[3], mac[4], mac[5]).ok();
 
     let device_id = Box::leak(device_id.into_boxed_str());
 
     let device = embassy_ha::new(
         RESOURCES.init(Default::default()),
-        embassy_ha::DeviceConfig { device_id, device_name: "Matrix", manufacturer: "Dragonn", model: "AWTRIX 3" },
+        embassy_ha::DeviceConfig { device_id, device_name: device_id, manufacturer: "Dragonn", model: "AWTRIX 3" },
     );
 
     let switch_indicator1 = embassy_ha::create_switch(
         &device,
         "ind1",
         embassy_ha::SwitchConfig {
-            common: embassy_ha::EntityCommonConfig { name: Some("Indicator 1"), ..Default::default() },
+            common: embassy_ha::EntityCommonConfig {
+                name: Some("Indicator 1"),
+                icon: Some("mdi:arrow-top-right-thick"),
+                ..Default::default()
+            },
+            class: embassy_ha::SwitchClass::Generic,
+            command_policy: embassy_ha::CommandPolicy::PublishState,
+        },
+    );
+
+    let switch_indicator2 = embassy_ha::create_switch(
+        &device,
+        "ind2",
+        embassy_ha::SwitchConfig {
+            common: embassy_ha::EntityCommonConfig {
+                name: Some("Indicator 2"),
+                icon: Some("mdi:arrow-right-thick"),
+                ..Default::default()
+            },
+            class: embassy_ha::SwitchClass::Generic,
+            command_policy: embassy_ha::CommandPolicy::PublishState,
+        },
+    );
+
+    let switch_indicator3 = embassy_ha::create_switch(
+        &device,
+        "ind3",
+        embassy_ha::SwitchConfig {
+            common: embassy_ha::EntityCommonConfig {
+                name: Some("Indicator 3"),
+                icon: Some("mdi:arrow-bottom-right-thick"),
+                ..Default::default()
+            },
             class: embassy_ha::SwitchClass::Generic,
             command_policy: embassy_ha::CommandPolicy::PublishState,
         },
     );
 
     spawner.must_spawn(switch_class(switch_indicator1));
+    spawner.must_spawn(switch_class(switch_indicator2));
+    spawner.must_spawn(switch_class(switch_indicator3));
 
     let mqtt_params =
         embassy_ha::MqttConnectParams { username: Some(MQTT_USER), password: Some(MQTT_PASSWORD.as_bytes()) };
@@ -51,7 +85,7 @@ pub async fn ha_task(spawner: Spawner, stack: embassy_net::Stack<'static>) {
     embassy_ha::connect_and_run(stack, device, MQTT_BROKER_ADDRESS, mqtt_params).await;
 }
 
-#[embassy_executor::task]
+#[embassy_executor::task(pool_size = 3)]
 async fn switch_class(mut switch: embassy_ha::Switch<'static>) {
     loop {
         let state = switch.toggle();
